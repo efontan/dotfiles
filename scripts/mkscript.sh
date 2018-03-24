eb12fa010984a25e5c451796343096d83bc54f69
Emmanuel Fontan <efontan@despegar.com>
2018-03-24 19:14:55 -0300

    Adding mkscript.sh fork from ActiveState recipe

---
+++ scripts/mkscript.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -e
+#title           :mkscript.sh
+#description     :This script will make a header for a bash script. This is a fork from https://github.com/ActiveState/code/tree/master/recipes/Bash/577862_Bash_script_create_header_Bash and modified by me to use vim by default.
+#originalauthor  :bgw
+#date            :20111101
+#version         :0.4    
+#usage		     :bash mkscript.sh
+#notes           :Install Vim and Emacs to use this script.
+#bash_version    :4.1.5(1)-release
+#==============================================================================
+
+today=$(date +%Y%m%d)
+div=======================================
+
+/usr/bin/clear
+
+_select_title(){
+
+    # Get the user input.
+    printf "Enter a title: " ; read -r title
+
+    # Remove the spaces from the title if necessary.
+    title=${title// /_}
+
+    # Convert uppercase to lowercase.
+    title=${title,,}
+
+    # Add .sh to the end of the title if it is not there already.
+    [ "${title: -3}" != '.sh' ] && title=${title}.sh
+
+    # Check to see if the file exists already.
+    if [ -e $title ] ; then 
+        printf "\n%s\n%s\n\n" "The script \"$title\" already exists." \
+        "Please select another title."
+        _select_title
+    fi
+
+}
+
+_select_title
+
+printf "Enter a description: " ; read -r dscrpt
+printf "Enter your name: " ; read -r name
+printf "Enter the version number: " ; read -r vnum
+
+# Format the output and write it to a file.
+printf "%-16s\n\
+%-16s%-8s\n\
+%-16s%-8s\n\
+%-16s%-8s\n\
+%-16s%-8s\n\
+%-16s%-8s\n\
+%-16s%-8s\n\
+%-16s%-8s\n\
+%-16s%-8s\n\
+%s\n\n\n" '#!/bin/bash -e' '#title' ":$title" '#description' \
+":${dscrpt}" '#author' ":$name" '#date' ":$today" '#version' \
+":$vnum" '#usage' ":./$title" '#notes' ':' '#bash_version' \
+":${BASH_VERSION}" \#$div${div} > $title
+
+# Make the file executable.
+chmod +x $title
+
+/usr/bin/clear
+vim +12 $title
+
