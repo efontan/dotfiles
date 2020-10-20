13c93a9c1136b65e19be4777f2fa236c516a67fa
Emmanuel Fontán <efontan@users.noreply.github.com>
2020-10-20 01:32:30 -0300

    Update gitCleanup.sh

--- scripts/gitCleanup.sh
+++ scripts/gitCleanup.sh
@@ -1,15 +1,27 @@
-#!/bin/bash  -e   
-#title          :git-cleanup.sh
+#!/bin/bash 
+#title          :gitCleanup.sh
 #description    :Cleans old branches and remotes from a local Git repository
 #author         :ef0ntan
 #date           :20180324
 #version        :0.0.1  
-#usage          :./git-cleanup.sh
+#usage          :./gitCleanup.sh develop
 #notes          :       
 #bash_version   :4.3.48(1)-release
 #============================================================================
+set -e
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
-MAIN_BRANCH="master"
+
+MAIN_BRANCH=$1
+
+if [[ -z "${MAIN_BRANCH}" ]]; then
+    echo -e "${RED}Main branch name required, please provide the main branch name. \nExample: ./gitCleanup.sh develop${NC}"
+    exit 1
+fi
+
 git checkout $MAIN_BRANCH -q
 git fetch -p
 
@@ -26,17 +38,19 @@ done
 # if there are branches to delete
 if [ ! ${#branches_to_delete[@]} -eq 0 ]; then
     # prompt for confirmation
-    echo "All this branches are no longer on remote: ${branches_to_delete[@]}"
-    echo -n -e "Are you sure you want to delete them? (y/n) "
-    read answer
-    echo
+    echo -e "All this branches are no longer on remote: ${YELLOW}${branches_to_delete[@]}${NC}"
+    read -p "$(echo -e ${YELLOW}"Are you sure you want to delete them? (y/n) "${NC})" -n 1 -r
+	echo
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then 
+	    echo "No branches where deleted."
+	    exit 0
+	fi
 
-    # delete
-    if echo "$answer" | grep -iq "^y" ;then
-        echo "${branches_to_delete[@]}" | xargs git branch -D
-        echo
-        echo "All local branches without a remote where deleted." 
-    fi
+     # delete
+    echo "${branches_to_delete[@]}" | xargs git branch -D
+    echo
+    echo -e "${GREEN}All local branches without a remote where deleted.${NC}" 
+
 else 
     echo "There are no local branches without a remote."
 fi
